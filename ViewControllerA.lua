4b3efcbf33a8d930da1668338a40a6a816a3cd75
jinhua <981479486@qq.com>
2015-06-30 15:56:19 +0800

    1
    1

--- ViewControllerA.lua
+++ ViewControllerA.lua
@@ -3,7 +3,7 @@ waxClass{"ViewControllerA", UIViewController}
 function viewDidLoad(self)
 	local view = UIView:initWithFrame(CGRect(50,100,200,200))
 	view:setBackgroundColor(UIColor:redColor())
-	self.view:addSubview(view)
+	self:view:addSubview(view)
 
 	-- body
 end
